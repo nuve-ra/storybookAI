0162f1f07b54610a574eed93d5c22eeab74f09de
Nupoor Verma <nuve19ec@cmrit.ac.in>
2025-08-04 20:52:57 +0530

    added ui

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@heroui/button";
 import { Input, Textarea } from "@heroui/input";
+import { Tooltip } from "@heroui/react";
 import React, { useState } from "react";
 
 function Page() {
@@ -11,12 +12,16 @@ function Page() {
 
   const handleSubmit = () => {
     // handle submission
+     setName("");
+    setEmail("");
+    setSubject("");
+    
   };
 
   return (
     <div
       className="min-h-screen bg-cover bg-center px-4  filter brightness-75"
-      style={{ backgroundImage: "url('/reader.jpg')" }}
+      style={{ backgroundImage: "url('/KidsBlogHJune.png')" }}
     >
       <h2 className="font-bold text-primary-100 text-2xl text-center p-10 mt-10">
         Say Hello to us!
@@ -24,7 +29,7 @@ function Page() {
       <h2 className="font-medium text-xl text-primary-200 text-center">
         Get in touch with your ideas! Send your idea of a story
       </h2>
-      <h3 className="text-center text-blue-850">kidzz@story.com</h3>
+      {/* <h3 className="text-center text-blue-850">kidzz@story.com</h3> */}
 
       <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-10 space-y-4">
         <Input
@@ -46,13 +51,17 @@ function Page() {
           className="border-b-2 w-full"
         />
         <Textarea placeholder="Your Message" className="w-full" />
-
+         {/* <Tooltip content="I am a tooltip" showArrow={true}></Tooltip>
         <Button
           className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg w-full"
-          radius="full"
+          radius="full" onClick={()=>alert("Submitted")}
         >
           Send Message
-        </Button>
+        </Button> */}
+        <Tooltip content="You can also reach us at kidzz@story.ac.in" showArrow={true}>
+      <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg w-full">Submit</Button>
+    </Tooltip>
+
       </form>
     </div>
   );
