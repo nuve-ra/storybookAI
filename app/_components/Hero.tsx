f839557613d499a3646ada71ba4b69e25c670c98
Nupoor Verma <139352147+nuve-ra@users.noreply.github.com>
2025-08-05 22:24:04 +0530

    Update Hero.tsx

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React from 'react';
 function Hero() {
   return (
-    <div className="px-10 md:px-28 lg:px-44 mt-10 h-screen">
+    <div className="px-10 md:px-28 lg:px-44 mt-10 min-h-screen">
       <div className="grid grid-cols-1 md:grid-cols-2 ">
         <div>
           <h2 className="text-[50px] text-primary font-extrabold py-10">
